13d1287cb219627eb285913dfebdff0d1ae75e18
Kush Pandya <kushpandya111@gmail.com>
2019-09-10 11:28:37 +0530

    Initial commit... With Fonts Widget, Images Widget, Button and AlertDialog Widget...

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'app_screens/home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Exploring UI widgets",
+      home: Home(),
+    )
+  );
+}
